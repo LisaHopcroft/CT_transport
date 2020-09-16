baabf8ce075b5ba4e7126e4a958324898f99276f
Lisa Hopcroft <Lisa.Hopcroft1@nhs.net>
2020-09-16 16:04:46 +0100

    Separating the generating (via PostcodesioR) and mapping (via leaflet) of postcodes.

--- 01_generate-random-postcodes.R
+++ 01_generate-random-postcodes.R
@@ -1,7 +1,9 @@
-library( tidyverse    )
+library( dplyr    )
+library( magrittr )
+library( ggplot2 )
+library( stringr )
 library( PostcodesioR )
-library( leaflet )
-library( rgdal )
+
 
 set.seed( "5482" )
 
@@ -69,70 +71,9 @@ postcode_holder = postcode_holder %>%
   arrange( id ) %>% 
   mutate( group = ifelse( is.na(id), "Hospital", "Participant") )
 
-centre_longitude  = postcode_holder %>% pull( longitude ) %>% mean
-centre_latitude   = postcode_holder %>% pull( latitude  ) %>% mean
-
-# https://rstudio.github.io/leaflet/markers.html
-
-# participant_icon <- makeIcon(
-#   iconUrl = "img/participant_grey.png",
-#   iconWidth = 38, iconHeight = 95,
-#   iconAnchorX = 22, iconAnchorY = 94#,
-#   #shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-#   #shadowWidth = 50, shadowHeight = 64,
-#   #shadowAnchorX = 4, shadowAnchorY = 62
-# )
-
-
-# Create a palette that maps factor levels to colors
-#pal <- colorFactor(c("navy", "red"), domain = c("ship", "pirate"))
-
-# leaflet(df) %>% addTiles() %>%
-#   addCircleMarkers(
-#     radius = ~ifelse(type == "ship", 6, 10),
-#     color = ~pal(type),
-#     stroke = FALSE, fillOpacity = 0.5
-#   )
-# 
-# leaflet(data = quakes[1:4,]) %>% addTiles() %>%
-#   addMarkers(~long, ~lat, icon = greenLeafIcon)
-
-postcode_areas = readOGR("/conf/Clinical_Trials_Unit/01 Projects/PROJECT_DSAP2020/6050105/postcode_polygons.gpkg", layer = "postcode_district")
-#postcode_areas = readOGR(dsn="Distribution", layer="Areas")
-layers <- ogrListLayers("../6050105/postcode_polygons.gpkg")
-
-
-postcode_areas = spTransform(postcode_areas, CRS("+proj=longlat +datum=WGS84"))
-
-postcode_areas = subset(postcode_areas, grepl("PA", postcodes$pc_district))
-
-
-
-postcode_areas = readOGR(dsn="Distribution", layer="Areas")
-
-marker_palette = colorFactor( c("navy","red"),
-                              domain = c("Hospital","Participant"))
-
-
-leaflet(postcode_holder) %>%
-  setView( lng=centre_longitude,
-           lat=centre_latitude,
-           zoom = 10 ) %>%
-  addTiles() %>% 
-  #addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
-  # addMarkers(~longitude,
-  #            ~latitude,
-  #            popup = ~as.character(id),
-  #            label = ~as.character(postcode))
-  addCircleMarkers(
-    ~longitude,
-    ~latitude,
-    radius = ~ifelse(group == "Hospital",20, 5),
-    color  = ~marker_palette(group),
-    stroke = TRUE,
-    fillOpacity = 0.5
-  )
-
+save( postcode_holder,
+      file=sprintf( "dat/%d_random_postcodes.Rdat",
+                    number_of_participants ) )
 
 postcode_holder %>% 
   filter( group == "Participant" ) %>% 
