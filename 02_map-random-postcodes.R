15ddfd9ea0cdd95ca4e3217373ea5fb44db8d033
Lisa Hopcroft <Lisa.Hopcroft1@nhs.net>
2020-10-12 12:34:09 +0100

    Small updates (including new file name for n=2000).

--- 02_map-random-postcodes.R
+++ 02_map-random-postcodes.R
@@ -1,17 +1,13 @@
 library( dplyr    )
-
-#library( data.table )
 library( rgdal ) 
-#library( htmltools )
-#library( htmlwidgets )
-
 library( magrittr )
 library( ggplot2 )
 library( stringr )
 library( leaflet )
 library( purrr )
+library( sp )
 
-load( "dat/20_random-postcodes.Rdat")
+load( "dat/01_RANDOM-POSTCODES_n=2000.Rdat")
 
 postcode_holder = postcode_holder %>% 
   mutate( Postcode = postcode )
@@ -31,7 +27,8 @@ leaflet(postcode_holder) %>%
   setView( lng=centre_longitude,
            lat=centre_latitude,
            zoom = 10 ) %>%
-  addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
+  #addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
+  addProviderTiles( providers$Stamen.TonerLite ) %>%
   addCircleMarkers(
     ~longitude,
     ~latitude,
@@ -48,16 +45,16 @@ leaflet(postcode_holder) %>%
 
 ### Read in shapefiles, the output is an object of the class
 ### SpatialPolygonsDataFrame
-postcode.objects = readOGR( dsn = "NRS_shapefiles/spd-unit-boundaries-cut-19-1", layer = "PC_Cut_19_1" )
+postcode.objects.in = readOGR( dsn = "NRS_shapefiles/spd-unit-boundaries-cut-19-1", layer = "PC_Cut_19_1" )
 ### Transform data in shapefile to the necessary format
-postcode.objects = spTransform( postcode.objects, CRS( "+proj=longlat +datum=WGS84" ) )
+postcode.objects.sp = spTransform( postcode.objects.in, CRS( "+proj=longlat +datum=WGS84" ) )
 
 ### Identify the postcode objects that we are interested in
 ### and retain only the data in the SpatialPolygonsDataFrame that
 ### is relevant for the areas of interest 
-postcode.objects.outcodes = postcode.objects$Postcode %>% map_chr( ~str_replace(.x, " .*", "" ) )
-postcode.objects = subset( postcode.objects,
-                           postcode.objects.outcodes %in% list_of_outcodes )
+postcode.objects.sp.outcodes = postcode.objects.sp$Postcode %>% map_chr( ~str_replace(.x, " .*", "" ) )
+postcode.objects = subset( postcode.objects.sp,
+                           postcode.objects.sp.outcodes %in% list_of_outcodes )
 ### Set projection attributes
 proj4string( postcode.objects )
 
@@ -66,11 +63,12 @@ proj4string( postcode.objects )
 ### with the information from the shape files
 postcode.objects.PATIENTS <- subset( postcode.objects,
                                      postcode.objects$Postcode %in% postcode_holder$Postcode )
-postcode.objects.PATIENTS <- merge( postcode.objects.PATIENTS, postcode_holder, by = "Postcode" )
+postcode.objects.PATIENTS <- merge( postcode.objects.PATIENTS, postcode_holder, by = "Postcode",
+                                    duplicateGeoms = TRUE)
 
 icon.fa <- makeAwesomeIcon( icon = 'flag', markerColor = 'red', iconColor = 'black' )
 
-postcode_map = leaflet( postcode.objects ) %>% 
+leaflet( postcode.objects ) %>% 
   addProviderTiles( providers$Stamen.TonerLite ) %>%
   setView( lng=centre_longitude,
            lat=centre_latitude,
@@ -104,13 +102,12 @@ postcode_map = leaflet( postcode.objects ) %>%
   #            labFormat = labelFormat( prefix = "" ),
   #            opacity = 1 )
 
-save( postcode_map,
-      postcode.objects,
+save( postcode.objects,
       postcode.objects.PATIENTS,
       marker_palette,
       centre_longitude,
       centre_latitude,
-      file=sprintf( "dat/%d_postcode_map.Rdat",
+      file=sprintf( "dat/02_MAPPING-INFORMATION_n=%d.Rdat",
                     number_of_participants ) )
 
 
