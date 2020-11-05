7c528cd30d89f673631ed62222018de0abd000e6
Lisa Hopcroft <Lisa.Hopcroft@phs.scot>
2020-11-05 09:18:55 +0000

    Finalising script to check synthetic data.  Adding accompanying .Rmd to document the results.  Will combine these later.

--- 05b_check-synthetic-data.R
+++ 05b_check-synthetic-data.R
@@ -5,6 +5,8 @@ library( sigmoid )
 library( ggplot2 )
 library( purrr )
 library( stringr )
+library( cowplot )
+# library( tibble )
 
 
 load( "dat/05a_SYNTHETIC-DATA_n=1958.Rdat" )
@@ -49,40 +51,89 @@ load( "dat/05a_SYNTHETIC-DATA_n=1958.Rdat" )
 #   ylim(0,1) +
 #   theme_bw()
 
-ggplot( data=trial_data.synthetic.MODERATE,
-        aes( x = ATT.T0.public_time,
-             y = RAW.public_time,
-             group = ATT.T0.public_time )) + geom_boxplot()
-
-ggplot( data=trial_data.synthetic.MODERATE,
-        aes( x = ATT.T0.public_time_random,
-             y = RAW.public_time_random,
-             group = ATT.T0.public_time_random )) + geom_boxplot()
-
-ggplot( data = map.vars.MODERATE,
-        aes( x=RAW.public_time,
-             y=MAP.public_time)) + geom_point()
-
-ggplot( data=trial_data.synthetic.EXTREME,
-        aes( x = ATT.T0.public_time,
-             y = RAW.public_time,
-             group = ATT.T0.public_time )) + geom_boxplot()
-
-ggplot( data=trial_data.synthetic.EXTREME,
-        aes( x = ATT.T0.public_time_random,
-             y = RAW.public_time_random,
-             group = ATT.T0.public_time_random )) + geom_boxplot()
-
-ggplot( data = map.vars.EXTREME,
-        aes( x=RAW.public_time,
-             y=MAP.public_time)) + geom_point()
-
 # 
-# save( trial_data.synthetic,
-#       trial_data.synthetic.EXTREME,
-#       map.vars.EXTREME,
-#       trial_data.synthetic.MODERATE,
-#       map.vars.MODERATE,
-#       file=sprintf( "dat/05a_SYNTHETIC-DATA_n=%d.Rdat",
-#                     number_of_participants ) )
+# this_data = trial_data.synthetic.EXTREME 
+# this_mapping = map.vars.EXTREME 
 
+
+RAW.columns = trial_data.synthetic.EXTREME %>% colnames %>% keep( ~str_detect(.x, "^RAW\\." ) )
+ATT.columns = trial_data.synthetic.EXTREME %>% colnames %>% keep( ~str_detect(.x, "^ATT\\." ) )
+
+sets_to_check = expand.grid( RAW.columns, ATT.columns ) %>% 
+  set_names( "input", "output") %>% 
+  mutate( metric = str_replace( input, "RAW\\.", "" ) ) %>% 
+  filter( str_detect( output, sprintf( "%s$", metric ) ) )
+
+for( dataset_type in c( "EXTREME",
+                        "MODERATE" ) ) {
+  
+  this_data.name = sprintf( "trial_data.synthetic.%s", dataset_type )
+  this_mapping.name = this_data.name %>% str_replace( "trial_data.synthetic",
+                                                      "map.vars")
+  
+  this_data = get( this_data.name )
+  this_mapping = get( this_mapping.name )
+  
+  
+  for ( i in 1:nrow(sets_to_check) ) {
+    
+    input_variable  = sets_to_check$input[i]  %>% as.character()
+    output_variable = sets_to_check$output[i] %>% as.character()
+    mapping_variable = input_variable %>% str_replace( "RAW", "MAP" )
+    this_timepoint = ( output_variable %>% str_match( "^.*\\.(T\\d{1})\\..*$" ) ) [2]
+    
+    i.d = this_data %>% select( idnum,
+                                {{input_variable}},
+                                {{output_variable}}) %>% 
+      inner_join( this_mapping %>% select( idnum,
+                                           {{mapping_variable}}),
+                  by="idnum")
+    
+    p1 = ggplot( i.d,
+                 aes( x=!!sym(input_variable),
+                      y=!!sym(mapping_variable)) ) +
+      geom_point() +
+      geom_line()
+    
+    p2 = ggplot( i.d,
+                 aes( x=!!sym(output_variable),
+                      y=!!sym(input_variable),
+                      group=!!sym(output_variable)
+                 ) ) +
+      geom_boxplot( )
+    
+    plot_title <- ggdraw() +
+      draw_label(
+        sprintf("Mapping from %s to attendance at %s (%s)",
+                input_variable,
+                this_timepoint,
+                dataset_type),
+        fontface = 'bold',
+        x = 0,
+        hjust = 0
+      ) +
+      theme(
+        # add margin on the left of the drawing canvas,
+        # so title is aligned with left edge of first plot
+        plot.margin = margin(0, 0, 0, 7)
+      )
+    
+    plot_row = plot_grid( p1, p2, labels=c("A", "B") )
+    
+    final_plot = plot_grid(
+      plot_title, plot_row,
+      ncol = 1,
+      rel_heights = c(0.1, 1)
+    )
+    
+    save_plot(
+      sprintf( "fig/05b_CHECK_%s-%s-%s.png",
+               input_variable,
+               this_timepoint,
+               dataset_type),
+      final_plot,
+      base_asp = 2)
+    
+  }
+  
+}
