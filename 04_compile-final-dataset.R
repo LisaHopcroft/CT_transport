3ed97b7b47d5e6aceb47acbb4ecb376342d8af93
Lisa Hopcroft <Lisa.Hopcroft@phs.scot>
2020-11-09 21:36:23 +0000

    Adding distance info from ArcPro.

--- 04_compile-final-dataset.R
+++ 04_compile-final-dataset.R
@@ -5,6 +5,11 @@ library( GGally )
 library( plyr )
 library( tidyr )
 library( ggbeeswarm )
+### conda install -c conda-forge r-matrix 
+### conda install -c r r-mgcv 
+### Sys.setenv(PKG_CONFIG_PATH = "/../miniconda3/pkgs/r-base-3.6.1-haffb61f_2/lib/R/lib")
+### install.packages("mgcv")
+library( mgcv )
 
 # load( "dat/00_PREPARATION.Rdat" )
 load( "dat/01a_RANDOM-POSTCODES_NEW_n=2000.Rdat" )
@@ -29,9 +34,10 @@ PUBLIC_journey_times.fastest.BJP = PUBLIC_journey_times.fastest %>%
 PRIVATE_journey_times.fastest.ArcPro = PRIVATE_journey_times.fastest.ArcPro %>% 
   dplyr::rename( idnum = OBJECTID ) %>% 
   dplyr::rename( private_time.ArcPro = Total_Drive ) %>% 
-  dplyr::rename( private_distance.ArcPro = Total_Length ) %>% 
+  dplyr::mutate( Total_Length_km = Total_Length/1000) %>% 
+  dplyr::rename( private_distance.ArcPro = Total_Length_km ) %>% 
   separate( Name, c("origin", "destination"), sep=" - " ) %>% 
-  select( origin, private_time.ArcPro ) %>% 
+  select( origin, private_time.ArcPro, private_distance.ArcPro ) %>% 
   unique()
   
 PRIVATE_journey_times.fastest.OSRM = PRIVATE_journey_times.fastest.OSRM %>% 
@@ -40,18 +46,17 @@ PRIVATE_journey_times.fastest.OSRM = PRIVATE_journey_times.fastest.OSRM %>%
   select( origin, private_time.OSRM ) %>% 
   unique()
 
-# asdf = PUBLIC_journey_times.fastest.BJP %>% 
-#   inner_join( PRIVATE_journey_times.fastest.ArcPro, by="idnum" ) %>% 
-#   inner_join( PRIVATE_journey_times.fastest.OSRM, by="idnum" ) %>% 
-#   mutate( error = (origin != origin.x) | (origin != origin.y)  )
 
 TRAVEL.data = PUBLIC_journey_times.fastest.BJP %>%
   inner_join( PRIVATE_journey_times.fastest.ArcPro, by="origin" ) %>%
   inner_join( PRIVATE_journey_times.fastest.OSRM, by="origin" ) %>%
   dplyr::rename( postcode = origin )
 
-# TRAVEL.data.toplot = TRAVEL.data %>% 
-#   pivot_longer( -origin, names_to="source",values_to="minutes")
+
+### Correlation between travel time from ArcPro and OSRM
+
+
+
 
 upper_limit = max( TRAVEL.data$private_time.ArcPro,
      TRAVEL.data$private_time.OSRM ) %>% round_any( 10 )
@@ -76,6 +81,29 @@ ggplot( TRAVEL.data,
   labs( title="Drive time (ArcPro v OSRM)",
         subtitle="Identity indicated by dashed blue line")
 
+
+### Correlation between travel time and travel distance
+
+upper_limit = max( TRAVEL.data$private_distance.ArcPro ) %>% round_any( 5 )
+
+ArcPro_time_distance.correlation = cor( TRAVEL.data$private_time.ArcPro,
+                               TRAVEL.data$private_distance.ArcPro )
+
+ggplot( TRAVEL.data,
+        aes(x=private_time.ArcPro,
+            y=private_distance.ArcPro)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  xlab( "Drive time (ArcPro)" ) +
+  ylab( "Drive distance (ArcPro)" ) +
+  annotate( "text",
+            x=0, y=upper_limit,
+            label = sprintf( "r=%.2f", ArcPro_time_distance.correlation ),
+            hjust=0, vjust=1 ) +
+  labs( title="Drive time ve distance (ArcPro)",
+        subtitle="Identity indicated by dashed blue line")
+
+### Correlation between private travel time and public travel time
 
 upper_limit = max( TRAVEL.data$private_time.ArcPro,
                    TRAVEL.data$public_time.BJP ) %>% round_any( 10 )
@@ -119,11 +147,7 @@ PATIENT.DATA.boxplot_data = PATIENT.DATA %>%
 ggplot( PATIENT.DATA.boxplot_data,
         aes( x=source,
              y=minutes )) +
-  geom_violin() #+
-  # geom_quasirandom( size=0.5 )
-  # geom_jitter( width=0.25)
-  # geom_beeswarm( )
-
+  geom_violin()
 
 save( PATIENT.DATA,
       file="dat/04_COMPILED-DATASET.Rdat" )
